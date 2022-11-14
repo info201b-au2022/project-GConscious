6429e53a567f299f9eca297a066c055f4322eac8
“aneC4” <noahc845@uw.edu>
2022-11-13 20:36:17 -0800

    updated summary.R

--- source/chart_2.R
+++ source/chart_2.R
@@ -12,19 +12,21 @@ data <- read.csv(filepath)
 filtered_data <- data %>% 
   filter(DATA_YEAR >= 2019) 
   
+#summarize data to top 10
 summarized_data <- filtered_data %>% 
   count(BIAS_DESC, sort = TRUE) %>% 
   top_n(n,n = 10) %>% 
+  #substitute long string
   mutate(BIAS_DESC = recode(BIAS_DESC,
                             "Anti-Lesbian, Gay, Bisexual, or Transgender (Mixed Group)" = "Anti-LGBT (Mixed Group)"
                             ))
 
-ggplot(summarized_data) +
+plot <- ggplot(summarized_data) +
   geom_col(
     mapping = aes(x= BIAS_DESC, y = n)
   )+
   coord_flip()+
-  theme_gray(base_size = 10)+
-  scale_x_discrete(labels = label_wrap(25)) +
+  theme_minimal(base_size = 10)+
+  scale_x_discrete(labels = label_wrap(20)) +
   scale_y_continuous(labels = comma) +
   labs(x = NULL, y = "Total Hate Crimes")
--- source/summary.R
+++ source/summary.R
@@ -1,1 +1,52 @@
 #Summary Information Script
+library(dplyr)
+
+filepath <- "./data/hate_crime.csv"
+data <- read.csv(filepath)
+
+#Filter data to only 2019/2020 data
+hc_2019_20 <- data %>% 
+  filter(DATA_YEAR >= 2019) 
+
+hc_2019 <- hc_2019_20 %>% 
+  filter(DATA_YEAR == 2019) 
+
+hc_2020 <- hc_2019_20 %>% 
+  filter(DATA_YEAR == 2020) 
+
+get_most_common <- function(df){
+  output <- df %>% 
+    count(BIAS_DESC, sort = TRUE) %>% 
+    filter(n == max(n)) %>% 
+    pull(BIAS_DESC)
+  
+  return(output)
+}
+
+get_percentage_most_common <- function(df){
+  output <- df %>% 
+    count(BIAS_DESC, sort = TRUE) %>% 
+    filter(n == max(n)) %>% 
+    pull(n)
+  
+  output <- output / nrow(df)
+    
+  return(output)
+}
+
+total_hate_crimes_2019_20 <- nrow(hc_2019_20)
+
+total_hate_crimes_2019 <- nrow (hc_2019)
+
+total_hate_crimes_2020 <- nrow (hc_2020)
+
+most_common_2019 <- get_most_common(hc_2019)
+  
+most_common_2020 <- get_most_common(hc_2020)
+
+percentage_most_common_2019 <- get_percentage_most_common(hc_2019)
+  
+percentage_most_common_2020 <- get_percentage_most_common(hc_2020)
+
+
+  
