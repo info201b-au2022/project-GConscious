6015649e25d5907249f1b91286efd8d94a86a9c7
“aneC4” <noahc845@uw.edu>
2022-11-13 21:06:06 -0800

    updated table.r

--- source/table.R
+++ source/table.R
@@ -1,1 +1,18 @@
 # Aggregate Table Script
+library(dplyr)
+
+filepath <- "./data/hate_crime.csv"
+data <- read.csv(filepath)
+
+filtered_data <- data %>% 
+  filter(DATA_YEAR >= 2019) 
+
+aggregate_table <- filtered_data %>% 
+  group_by(STATE_NAME) %>% 
+  tally() %>% 
+  table()
+
+colnames(aggregate_bable) = c("State", "Hate Crimes (2019-2020)") 
+
+
+  
