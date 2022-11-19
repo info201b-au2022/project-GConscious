1cc4039c1d57487f140aca239f316961558401cb
GConscious <amrithgandham@gmail.com>
2022-11-18 23:36:34 -0800

    Complete chart_3

--- source/chart_3.R
+++ source/chart_3.R
@@ -1,22 +1,33 @@
+#Chart 3 Line Chart
+
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
 
 hate_crimes <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-GConscious/main/data/hate_crime_filtered.csv")
 
-hc_2019_20 <- data %>% 
-  filter(DATA_YEAR >= 2019) 
+formatted <- as.Date.character(hate_crimes$INCIDENT_DATE)
+months <- format(formatted, "%b")
 
-hc_2019 <- hc_2019_20 %>% 
-  filter(DATA_YEAR == 2019) 
+hate_crimes <- hate_crimes %>% 
+  mutate("month_name" = months)
 
-hc_2020 <- hc_2019_20 %>% 
+hc_2020 <- hate_crimes %>% 
   filter(DATA_YEAR == 2020) 
 
-g_range<- (0, hc_2019, hc_2020)
+filtered_months <- hc_2020 %>% 
+  select(month_name) %>% 
+  group_by(month_name) %>% 
+  tally() %>% 
+  rename("counts" = "n")
 
-ggplot(data=hc_2019, aes(x=time, y=number_of_hate_crimes)) +
-  geom_line()
-ggplot(data=hc_2020, aes(x=time, y=number_of_hate_crimes)) +
-  geom_line()
-
+month_counts_2020 <- ggplot(data = filtered_months, aes(x = month_name, y = counts, group = 1)) +
+  geom_line() +
+  scale_y_continuous(limits = c(450, 1450), breaks = seq(400, 1450, 200)) +
+  geom_point() +
+  labs(
+    title = "Number of Hate Crimes Per Month In All States",
+    x = "Month",
+    y = "Total Number of Hate Crimes"
+  )
+  
