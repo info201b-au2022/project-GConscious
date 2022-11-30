5b17b84be4b8786d60ad24e2fffa96ca7c174899
GConscious <amrithgandham@gmail.com>
2022-11-29 22:37:32 -0800

    New changes

--- source/chart_3.R
+++ source/chart_3.R
@@ -16,12 +16,17 @@ hc_2020 <- hate_crimes %>%
   filter(DATA_YEAR == 2020) 
 
 filtered_months <- hc_2020 %>% 
-  select(month_name) %>% 
+  select(month_name) %>%
   group_by(month_name) %>% 
   tally() %>% 
   rename("counts" = "n")
 
-month_counts_2020 <- ggplot(data = filtered_months, aes(x = month_name, y = counts, group = 1)) +
+ordered_months <- filtered_months %>% 
+  mutate(month_name = factor(month_name, levels = month.abb)) %>% 
+  arrange(month_name)
+
+
+month_counts_2020 <- ggplot(data = ordered_months, aes(x = month_name, y = counts, group = 1)) +
   geom_line() +
   scale_y_continuous(limits = c(450, 1450), breaks = seq(400, 1450, 200)) +
   geom_point() +
